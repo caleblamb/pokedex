648d3e2e048e03abab9ce6b571d5ac28140056dc
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-14 19:47:00 +0000

    fixed about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,5 +2,5 @@
     <title>About Pokedex</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase>About</h1>
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
 <p>This is a SvelteKit demo showcasing a Pokemon index.</p>
