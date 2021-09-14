5d251eebafaf0dba79219f1f77519e6e6fc58dd8
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-14 19:44:19 +0000

    final sveltekit demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {pokemon} from "../stores/pokestore";
+    import PokemanCard from "../components/pokemanCard.svelte";
+    let searchTerm = "";
+    let filteredPokemon = [];
+    $: {
+        if(searchTerm){
+            filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        }
+        else {
+            filteredPokemon = [...$pokemon];
+        }
+    }
+</script>
+<svelte:head>
+	<title>Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Pokemon">
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    
+    {#each filteredPokemon as pokeman}
+            <PokemanCard pokeman={pokeman}/>
+    {/each}
+</div>
